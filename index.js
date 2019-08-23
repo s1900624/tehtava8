3daadb5fe5c3dfa50d74e21cc43433417e54a0e0
Ilmast Toivo <s1900624@edu.bc.fi>
2019-08-23 14:04:23 +0300

    Update: other variant of transform button onclick function

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ window.onload = function() {
 
     transform.onclick = () => {
         const str = word.value;
+        /*
         if (str !== '') {
-            console.log(str.replace(/e/gi, 'o'));
+           console.log(str.replace(/e/gi, 'o'));
         }
+        */
+        str !== '' ? console.log(str.replace(/e/gi, 'o')) : '';
     }
 };
